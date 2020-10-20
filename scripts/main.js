141dcc95f3e50cbd2e086ab2c31cfb63fd483f56
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-20 11:48:13 -0500

    Refactored the code to decouple components by using and eventHub custom event

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 import { MessageList } from "./messages/MessageList.js"
+import { ThemeButtons } from "./themes/ThemeButtons.js"
 
 MessageList()
+ThemeButtons()
 
--- scripts/messages/MessageList.js
+++ scripts/messages/MessageList.js
@@ -23,3 +23,30 @@ const render = messageArray => {
     contentTarget.innerHTML = combinedSections
 }
 
+/*
+    Color the messages when one of the buttons in the ThemeButtons
+    component is clicked.
+*/
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("colorChosen", event => {
+    const color = event.detail.color
+
+    contentTarget.classList = []
+    contentTarget.classList.add(color)
+})
+
+/*
+    Color the messages when one of the buttons in the ThemeButtons
+    component is clicked.
+*/
+// document.querySelector(".themes").addEventListener("click", e => {
+//     const idOfClickedElement = e.target.id
+
+//     if (idOfClickedElement.startsWith("themeButton--")) {
+//         const [prefix, color] = idOfClickedElement.split("--")
+//         contentTarget.classList = []
+//         contentTarget.classList.add(color)
+//     }
+// })
+
